be5e7bc0f352c60e971127db73010ca1b42798ea
wangsongwen <2544894086wsw@gmail.com>
2021-03-05 17:44:43 +0800

    first commit

---
+++ src/request/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import url from './url';
+
+function axiosRequest(){
+  axios.post('/user', {
+    firstName: 'Fred',
+    lastName: 'Flintstone'
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
---
+++ src/request/url.js
@@ -0,0 +1,3 @@
+let taskServiceUrl = '127.0.0.1:4001';
+
+export default taskServiceUrl;
